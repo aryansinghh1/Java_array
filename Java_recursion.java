96be33278294af432be34eeebee2d2d648b7fb36
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-09 11:11:00 +0530

    recursion basics

---
+++ Java_recursion.java
@@ -0,0 +1,24 @@
+public class Java_recursion {
+    public static int sum(int k) {
+        if (k > 0) {
+            return k + sum(k - 1);
+        } else {
+            return 0;
+        }
+    }
+
+    static void countdown(int n) {
+    if (n > 0) {
+      System.out.print(n + " ");
+      countdown(n - 1);
+    }
+  }
+
+    public static void main(String[] args) {
+        int result = sum(10);
+        System.out.println(result);
+
+        countdown(5);
+    }
+
+}
