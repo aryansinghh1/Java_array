451dd9763b84ede029ccd30a902b76913001cbc3
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-09 10:02:56 +0530

    methods

--- Method_ifelse.java
+++ Method_ifelse.java
@@ -2,8 +2,14 @@ public class Method_ifelse {
 
     static void myMethod(int age){
 
+        if(age < 18){
+            System.out.println("person in minor");
+        }
+        else{
+            System.out.println("he is not minor");
+        }
     }
     public static void main(String[] args) {
-        
+        myMethod(18);
     }
 }
