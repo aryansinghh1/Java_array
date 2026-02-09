9809f8307b66079f7a3ecf46d23125949093fa39
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-09 10:13:51 +0530

    int method

---
+++ Method_int.java
@@ -0,0 +1,12 @@
+public class Method_int {
+    static int myMethod(int x,int y) {
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(myMethod(9,9));
+        int result  = myMethod(8,9);
+    
+        System.out.println(result);
+    }
+}
